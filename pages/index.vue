4a9c9e57043f2d308d8bbd2b7e99904ba6d68446
mlarese <mauro.larese@gmail.com>
2019-10-28 13:00:20 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="iframe-container">
+    <iframe :src="src" />
+  </div>
+</template>
+
+<script>
+// import AsyncIframe from 'vue-friendly-iframe'
+
+export default {
+  // components: { AsyncIframe },
+  data () {
+    return {
+      src: 'https://www.bing.com'
+    }
+  },
+  methods: {
+    onLoad () {
+
+    },
+    onDocumentLoad () {
+
+    }
+  }
+}
+</script>
